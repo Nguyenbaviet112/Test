cbf7a8cbb5ff153c8ace7895683918778a7e8fc5
Nguyễn Bá Việt <51191112+Nguyenbaviet112@users.noreply.github.com>
2022-08-23 20:52:03 +0700

    Update sotrepprocedure PhanTom

--- StoreProc_Trans/21424069/PHANTOM/phantom.sql
+++ StoreProc_Trans/21424069/PHANTOM/phantom.sql
@@ -1,28 +1,15 @@
 ﻿CREATE 
 --ALTER
-PROC USP_21424069_READ_DATA_Orders
-	@ORDERS_ID BIGINT
-
+PROC USP_21424069_READ_DATA_USERS
 AS
-SET TRAN ISOLATION LEVEL REPEATABLE READ
 BEGIN TRAN
 	BEGIN TRY
-	IF NOT EXISTS (SELECT * 
-				FROM Orders O
-				WHERE O.ID = @ORDERS_ID )
-	BEGIN
-		PRINT N' Không Tồn Tại Đơn Hàng'
-		ROLLBACK TRAN
-		RETURN 1
-	END
 
-	
-
-	SELECT Amount FROM Orders WHERE ID = @ORDERS_ID 
+	SELECT COUNT(*) FROM Users U WHERE U.RoleID = 2
 
 	WAITFOR DELAY '0:0:10'
 	
-	SELECT * FROM OrderDetails  WHERE OrderID = @ORDERS_ID 
+	SELECT * FROM Users U WHERE U.RoleID = 2
 
 	
 	END TRY
@@ -37,19 +24,21 @@ GO
 
 CREATE 
 --ALTER
-PROC USP_21424069_WRITE_DATA_OrdersDetail
-	@ID BIGINT,
-	@ORDERS_ID BIGINT,
-	@PRODUCT_ID BIGINT,
-	@QUANTITY INT,
-	@PRICE DECIMAL(18,2),
-	@AMOUNT DECIMAL(18,2)
+PROC USP_21424069_INSERT_DATA_USERS
+	@FullName nvarchar(max),
+	@Phone varchar(12),
+	@Address nvarchar(MAX),
+	@Email nvarchar(50),
+	@NameOfBank nvarchar(200),
+	@BankAccount varchar(50),
+	@NumOfOrder int,
+	@RoleID int
 AS
 BEGIN TRAN
 	BEGIN TRY
 
-		INSERT INTO OrderDetails
-		VALUES(@ORDERS_ID, @PRODUCT_ID, @QUANTITY, @PRICE, @AMOUNT)
+		INSERT INTO Users(FullName, Phone, Address, Email, NameOfBank, BankAccount, NumOfOrder, RoleID)
+		VALUES(@FullName, @Phone, @Address, @Email, @NameOfBank, @BankAccount, @NumOfOrder, @RoleID)
 
 
 	END TRY
@@ -61,4 +50,3 @@ BEGIN TRAN
 COMMIT TRAN
 RETURN 0
 GO
-
